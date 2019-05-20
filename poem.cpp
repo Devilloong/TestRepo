17c0a35262c19b15d366ed0a58071d73a62914b6
Devilloong <45351664+Devilloong@users.noreply.github.com>
2019-05-20 11:30:34 +0900

    add the second lines

--- poem.cpp
+++ poem.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
   std::cout << "Hello world." << std::endl;
-  
+  std::cout << "add the line which is not necessary." << std::endl;
   while(std::cin.get() != '\n')
   return 0;
 }
